fb0b76d26229e01b0427c55d9eab2344a0917d04
Ghostlyquack <45326071+Ghostlyquack@users.noreply.github.com>
2020-04-23 16:54:48 -0700

    Main Browser

---
+++ Nebula.vb
@@ -0,0 +1,26 @@
+﻿Public Class Nebula
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate("www.google.ca")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WebBrowser1.GoBack()
+
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WebBrowser1.GoForward()
+
+
+    End Sub
+End Class
